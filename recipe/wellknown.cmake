97766c63b86c6bb31df2a5babb46e05cfccf1b7c
Noah Watkins <noahwatkins@gmail.com>
2019-03-14 19:44:43 -0700

    build: remove cooking bits
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- recipe/wellknown.cmake
+++
@@ -1,9 +0,0 @@
-cooking_ingredient(smf
-  COOKING_RECIPE wellknown
-  COOKING_CMAKE_ARGS
-    -DSMF_ENABLE_TESTS=OFF
-    -DSMF_BUILD_PROGRAMS=OFF
-  EXTERNAL_PROJECT_ARGS
-    GIT_REPOSITORY https://github.com/smfrpc/smf.git
-    GIT_TAG dfe2113
-    )
